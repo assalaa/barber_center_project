6329c2b8267a674291162ff023fb072e52118763
arielsam567 <Saidemim112104>
2023-04-17 08:14:10 -0300

    FIX BUS

---
+++ lib/database/db_salon_service.dart
@@ -0,0 +1,22 @@
+import 'package:barber_center/models/saloon_service_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseSalonService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String _path = 'salon_service';
+
+  Future<void> createService(SalonServiceModel salonServiceModel) async {
+    await _firestore.collection(_path).doc(salonServiceModel.userId).set(salonServiceModel.toJson());
+  }
+
+  Future<SalonServiceModel> getServicesByUserId(String id) async {
+    final DocumentSnapshot snapshot = await _firestore.collection(_path).doc(id).get();
+    final Map map = snapshot.data() as Map;
+    final SalonServiceModel salonServiceModel = SalonServiceModel.fromJson(map);
+    return salonServiceModel;
+  }
+
+  Future<void> updateService(SalonServiceModel salonServiceModel) async {
+    await _firestore.collection(_path).doc(salonServiceModel.userId).update(salonServiceModel.toJson());
+  }
+}
