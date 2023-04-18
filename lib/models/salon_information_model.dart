05ec123310a63093652a7ba5b7cbab7fc512d22d
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-04-18 16:40:15 +0200

    merge2

--- lib/models/salon_information_model.dart
+++ lib/models/salon_information_model.dart
@@ -1,8 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-=======
->>>>>>> 27d418784bccd97c002e29704ad0efc7366fc597
 
 class SalonInformationModel {
   final String salonId;
@@ -26,15 +22,9 @@ class SalonInformationModel {
       salonId: json['salonId'],
       address: json['address'],
       chairs: json['chairs'],
-<<<<<<< HEAD
-      openTime: json['openTime'].toDate().toLocal(),
-      closeTime: json['closeTime'].toDate().toLocal(),
-      employees: List<String>.from(json['employees']),
-=======
       openTime: json['openTime'].toDate(),
       closeTime: json['closeTime'].toDate(),
       employees: List<String>.from(json['employees'].map((x) => x)),
->>>>>>> 27d418784bccd97c002e29704ad0efc7366fc597
     );
   }
 
@@ -44,10 +34,6 @@ class SalonInformationModel {
         'chairs': chairs,
         'openTime': Timestamp.fromDate(openTime.toUtc()),
         'closeTime': Timestamp.fromDate(closeTime.toUtc()),
-<<<<<<< HEAD
-        'employees': List<dynamic>.from(employees.map((e) => e)),
-=======
         'employees': List<dynamic>.from(employees.map((x) => x)),
->>>>>>> 27d418784bccd97c002e29704ad0efc7366fc597
       };
 }
