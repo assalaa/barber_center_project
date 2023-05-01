256023c98439c8688a41c5e674cd0ccc9d373722
assala.dhouioui <assala.dhouioui@aiesec.net>
2023-05-01 23:16:44 +0200

    barber home page- appointement section

--- lib/screens/barber/home_barber_screen/home_barber_provider.dart
+++ lib/screens/barber/home_barber_screen/home_barber_provider.dart
@@ -1,3 +1,28 @@
+import 'package:barber_center/database/db_auth.dart';
+import 'package:barber_center/database/db_profile.dart';
+import 'package:barber_center/models/user_model.dart';
+import 'package:barber_center/services/routes.dart';
 import 'package:flutter/cupertino.dart';
 
-class HomeBarberProvider with ChangeNotifier {}
+class HomeBarberProvider with ChangeNotifier {
+  final DatabaseUser _dbUsers = DatabaseUser();
+  final DatabaseAuth _dbAuth = DatabaseAuth();
+  bool loading = true;
+  late Locale myLocale;
+  late UserModel userModel;
+
+  HomeBarberProvider() {
+    init();
+  }
+  Future<void> init() async {
+    await Future.wait([getCurrentBarber()]);
+    myLocale = Localizations.localeOf(Routes.navigator.currentContext!);
+    loading = false;
+    notifyListeners();
+  }
+
+  Future<void> getCurrentBarber() async {
+    final String uid = _dbAuth.getCurrentUser()!.uid;
+    userModel = (await _dbUsers.getUserByUid(uid))!;
+  }
+}
--- lib/screens/barber/home_barber_screen/home_barber_screen.dart
+++ lib/screens/barber/home_barber_screen/home_barber_screen.dart
@@ -1,14 +1,164 @@
+import 'package:barber_center/screens/barber/home_barber_screen/home_barber_provider.dart';
+import 'package:barber_center/services/language_constants.dart';
+import 'package:barber_center/utils/app_layout.dart';
+import 'package:barber_center/utils/app_styles.dart';
+import 'package:barber_center/widgets/center_loading.dart';
+import 'package:barber_center/widgets/section_header.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
 
 class HomeBarberScreen extends StatelessWidget {
   const HomeBarberScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Barber Screen'),
+    return ChangeNotifierProvider<HomeBarberProvider>(
+      create: (context) => HomeBarberProvider(),
+      child: Consumer<HomeBarberProvider>(
+        builder: (context, provider, _) {
+          return Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              toolbarHeight: 0,
+            ),
+            body: Column(
+              children: [
+                if (provider.loading) ...[
+                  const CenterLoading()
+                ] else ...[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //SEARCH BAR
+                        (provider.myLocale == const Locale(ENGLISH, ''))
+                            ? const Text(
+                                'El-Mezayen',
+                                style: TextStyle(color: Colors.black, fontFamily: 'DancingScript', fontSize: 40),
+                              )
+                            : const Text(
+                                'المزين',
+                                style: TextStyle(color: Colors.black, fontFamily: 'decotype', fontSize: 40),
+                              ),
+                        //NOTIFICATION ICON
+                        Stack(
+                          children: [
+                            Container(
+                              width: AppLayout.getHeight(50),
+                              height: AppLayout.getWidth(50),
+                              decoration: BoxDecoration(image: const DecorationImage(image: NetworkImage('provider.userModel.image')), borderRadius: BorderRadius.circular(100), color: Styles.greyColor.withOpacity(0.2)),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+                Gap(AppLayout.getHeight(45)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                  ),
+                  child: SectionHeader(
+                    sectionTitle: AppLocalizations.of(context)!.featured_salons,
+                    sectionSeeMore: '',
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (BuildContext context, int index) {
+                        return const Padding(
+                          padding: EdgeInsets.all(8),
+                          child: appointement_card(),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
+  }
+}
+
+class appointement_card extends StatelessWidget {
+  const appointement_card({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Gap(AppLayout.getWidth(5)),
+      const Text(
+        '11:00 AM',
+        style: TextStyle(color: Styles.darkBlueColor, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      Container(
+        padding: const EdgeInsets.all(15),
+        width: 270,
+        height: 100,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: const Offset(0, 2), // changes position of shadow
+            ),
+          ],
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Customer Image
+            Row(
+              children: [
+                const CircleAvatar(
+                    radius: 30.0,
+                    backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                    )),
+                Gap(AppLayout.getWidth(15)),
+                //Customer name
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Name last name',
+                      style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Gap(AppLayout.getHeight(5)),
+                    Row(
+                      children: const [
+                        Icon(
+                          FluentSystemIcons.ic_fluent_location_filled,
+                          size: 20,
+                          color: Styles.primaryColor,
+                        ),
+                        Text(
+                          'Customer address ',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+
+                //Address
+              ],
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }
